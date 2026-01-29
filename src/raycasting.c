6a58b7e0fb5ad3c82cd43c095f0de4962ae0e4eb
atashiro <galaxyfish11@gmail.com>
2026-01-29 14:26:11 +0900

    東西南北に色を適応

--- src/raycasting.c
+++ src/raycasting.c
@@ -6,22 +6,49 @@
 /*   By: atashiro <atashiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 17:38:32 by atashiro          #+#    #+#             */
-/*   Updated: 2026/01/28 14:47:40 by atashiro         ###   ########.fr       */
+/*   Updated: 2026/01/29 14:24:17 by atashiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3D.h"
 
-static void	draw_debug_distance(t_game *game, t_ray *ray, int x)
+// static void	draw_debug_distance(t_game *game, t_ray *ray, int x)
+// {
+// 	int	color_value;
+// 	int	color;
+// 	int	y;
+
+// 	color_value = 255 - (int)(ray->perp_wall_dist * 25);
+// 	if (color_value < 0)
+// 		color_value = 0;
+// 	color = (color_value << 16) | (color_value << 8) | color_value;
+// 	y = ray->draw_start;
+// 	while (y < ray->draw_end)
+// 	{
+// 		put_pixel(x, y, color, game);
+// 		y++;
+// 	}
+// }
+
+static void	draw_debug_compass(t_game *game, t_ray *ray, int x)
 {
-	int	color_value;
 	int	color;
 	int	y;
 
-	color_value = 255 - (int)(ray->perp_wall_dist * 25);
-	if (color_value < 0)
-		color_value = 0;
-	color = (color_value << 16) | (color_value << 8) | color_value;
+	if (ray->side == 1) // Y軸面 (南北)
+	{
+		if (ray->ray_dir_y < 0)
+			color = 0xFF0000; // 北 (Red)
+		else
+			color = 0x00FF00; // 南 (Green)
+	}
+	else // X軸面 (東西)
+	{
+		if (ray->ray_dir_x < 0)
+			color = 0x0000FF; // 西 (Blue)
+		else
+			color = 0xFFFF00; // 東 (Yellow)
+	}
 	y = ray->draw_start;
 	while (y < ray->draw_end)
 	{
@@ -50,7 +77,8 @@ int	raycasting(t_game *game)
 		init_ray(&ray, &vec, x);
 		perform_dda(game, &ray);
 		calc_wall_height(&ray);
-		draw_debug_distance(game, &ray, x);
+		// draw_debug_distance(game, &ray, x);
+		draw_debug_compass(game, &ray, x);
 		x++;
 	}
 	create_map(game);
