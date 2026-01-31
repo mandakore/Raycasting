5eb192995d79d922db84d982fe3982a701f0974b
SOHYAMAZ <snow.sohei@gmail.com>
2026-01-31 20:52:38 +0900

    wip(parse): work in progress

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: atashiro <atashiro@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/01/10 13:57:51 by sohyamaz          #+#    #+#              #
-#    Updated: 2026/01/28 09:17:37 by atashiro         ###   ########.fr        #
+#    Updated: 2026/01/31 18:32:50 by sohyamaz         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -42,7 +42,13 @@ SRCS	= src/main.c \
 		  src/free.c  \
 		  src/load_texture.c \
 		  src/minimap.c \
-		  src/init_raycast.c src/calcurate_wall_height.c src/dda.c src/init_player.c src/move.c src/move_calc.c
+		  src/init_raycast.c \
+		  src/calcurate_wall_height.c \
+		  src/dda.c \
+		  src/init_player.c \
+		  src/move.c \
+		  src/move_calc.c \
+		  src/parse.c
 
 OBJS	= $(SRCS:%.c=%.o)
 
--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,7 @@
 /*   By: sohyamaz <sohyamaz@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/31 18:28:27 by sohyamaz          #+#    #+#             */
-/*   Updated: 2026/01/31 20:12:06 by sohyamaz         ###   ########.fr       */
+/*   Updated: 2026/01/31 20:51:35 by sohyamaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,16 @@
 //inside
 //
 
+typedef struct s_map	t_map;
+
+struct s_map
+{
+	int		cabfd;
+	int		map_lenx;
+	int		map_leny;
+	char	**mapdata;
+}
+
 bool	parse(int argc, char **argv, t_game *game)
 {
 	if (argv == NULL || game == NULL)
@@ -30,8 +40,6 @@ bool	parse(int argc, char **argv, t_game *game)
 		return (false);
 	//func_filecheck
 	if (fd <= 1)
-		return (false);
-	if (parse_cab_file(fd, t_map) == false)
 		return (false);
 	if (is_wall_closed(t_map) == false)
 	{
@@ -44,4 +52,55 @@ bool	parse(int argc, char **argv, t_game *game)
 		return (false);
 	}
 	return (true);
+}
+
+int    open_if_file_is_valid(char *argv1)
+{
+    int        fd;
+    size_t    name_len;
+    char    *filename;
+
+    filename = ft_strrchr(argv1, '/');
+    if (filename)
+        filename++;
+    else
+        filename = argv1;
+    name_len = ft_strlen(filename);
+    if (name_len <= 4
+        || ft_strncmp(filename + name_len - 4, ".ber", 4) != 0
+        || name_len == 4)
+        error_exit("invalid extension");
+    fd = open(argv1, O_RDONLY);
+    if (fd == -1)
+        error_exit("failed to open file.");
+    return (fd);
+}
+
+static bool	is_valid_cabfile(char *cabfile)
+{
+	char	*filename;
+	size_t	fname_len;
+
+	if (cabfile == NULL)
+		return (false);
+	filename = ft_strrchr(cabfile, '/');
+	if (filename != NULL)
+		filename++;
+	else
+		filename = cabfile;
+	fname_len = ft_strlen(filename);
+	if (fname_len <= 4)
+		return (false);
+	extention = filename + fname_len - 4;
+	if (ft_strncmp(extention, ".cab", 4) != 0)
+		return (false);
+}
+
+static bool	parse_cabfile(t_map *map, char *cabfile)
+{
+	if (cabfile == NULL)
+		return (false);
+	if (is_valid_cabfile(cabfile) == false)
+		return (false);
+	if (is_texture_file_exist)
 }
