8bd0e4206479f00bd07620cd2c2561d01cd9ad08
mandakore <galaxyfish11@gmail.com>
2026-01-31 21:29:19 +0900

    汎用関数を追加

---
+++ src/minimap_utils.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minimap_utils.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atashiro <atashiro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/31 21:26:21 by atashiro          #+#    #+#             */
+/*   Updated: 2026/01/31 21:28:35 by atashiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/cub3D.h"
+
+int	get_line_len(char *line)
+{
+	int i;
+
+	i = 0;
+	if (!line)
+		return (0);
+	while (line[i])
+		i++;
+	return (i);
+}
+
+int	get_map_height(char **map)
+{
+	int i;
+
+	i = 0;
+	if (!map)
+		return (0);
+	while (map[i])
+		i++;
+	return (i);
+}
