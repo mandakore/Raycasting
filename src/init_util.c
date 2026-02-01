d94256ee4ccb002865a1813dac979bd1356cc476
SOHYAMAZ <snow.sohei@gmail.com>
2026-02-01 22:04:03 +0900

    feat(init): add func for count the sizeof map

---
+++ src/init_util.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_util.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sohyamaz <sohyamaz@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/01 19:41:27 by sohyamaz          #+#    #+#             */
+/*   Updated: 2026/02/01 22:02:01 by sohyamaz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cab3d.h"
+
+
+
+bool	get_map_size(t_map *map, char *fullpath)
+{
+	int		width;
+	char	*line;
+
+	if (map == NULL || fullpath == NULL)
+		return (false);
+	map->cabfd = open(fullpath, O_RONLY);
+	if (map->cabfd < 0)
+		return (perror("open"), false);
+	width = 0;
+	while (1)
+	{
+		line = get_next_line(map->cabfd);
+		if (line == NULL)
+			break ;
+		width = ft_strlen(line) + 2;
+		if (width > map->x)
+			map->x = width;
+		map->y++;
+		free(line)
+	}
+	map->y = height + 2;
+	close(cabfd);
+	return (true);
+}
