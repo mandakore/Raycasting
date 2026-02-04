c9c290b311edfd73a2fe96c30e831e601a62f825
mandakore <galaxyfish11@gmail.com>
2026-02-04 19:00:27 +0900

    関数を追加

--- src/minimap_utils.c
+++ src/minimap_utils.c
@@ -6,7 +6,7 @@
 /*   By: atashiro <atashiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/31 21:26:21 by atashiro          #+#    #+#             */
-/*   Updated: 2026/02/04 18:43:00 by atashiro         ###   ########.fr       */
+/*   Updated: 2026/02/04 18:53:23 by atashiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,4 +34,31 @@ int	get_map_height(char **map)
 	while (map[i])
 		i++;
 	return (i);
+}
+
+int	is_in_map(t_game *game, int x, int y)
+{
+	if (y < 0 || y >= get_map_height(game->map))
+		return (0);
+	if (x < 0 || x >= get_line_len(game->map[y]))
+		return (0);
+	return (1);
+}
+
+int	is_wall(t_game *game, int x, int y)
+{
+	if (game->map[y][x] == '1')
+		return (1);
+	return (0);
+}
+
+void	draw_minimap_wall(t_game *game, int x, int y)
+{
+	t_square	square;
+
+	square.x = MM_PLAYER_X + x * WALL;
+	square.y = MM_PLAYER_Y + y * WALL;
+	square.size = WALL;
+	square.color = 0xADFF2F;
+	draw_square(game, square);
 }
