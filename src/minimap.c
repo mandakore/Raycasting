31e39ecfd72e08f439cb19322796f92b0c797fef
mandakore <galaxyfish11@gmail.com>
2026-02-23 14:57:27 +0900

    埋まって見えるのを改善？

--- src/minimap.c
+++ src/minimap.c
@@ -6,7 +6,7 @@
 /*   By: atashiro <atashiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 17:33:14 by atashiro          #+#    #+#             */
-/*   Updated: 2026/02/21 13:07:58 by atashiro         ###   ########.fr       */
+/*   Updated: 2026/02/23 14:56:42 by atashiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,8 +37,8 @@ static void	draw_player(t_game *game)
 	player_square.x = MM_PLAYER_X + MM_RAD * WALL;
 	player_square.y = MM_PLAYER_Y + MM_RAD * WALL;
 	player_square.size = 6;
-	player_square.x += (WALL - player_square.size) / 2;
-	player_square.y += (WALL - player_square.size) / 2;
+	player_square.x -= (WALL - player_square.size) / 4;
+	player_square.y -= (WALL - player_square.size) / 4;
 	player_square.color = MM_PLAYER_COLOR;
 	draw_square(game, player_square);
 }
