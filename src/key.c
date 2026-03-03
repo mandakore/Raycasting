5562f0d20ee953d22ff99368b5ec4bff80912395
Tashiro Atsuya <galaxyfish11@gmail.com>
2026-03-03 17:18:07 +0900

    a

--- src/key.c
+++ src/key.c
@@ -12,7 +12,8 @@
 
 #include "../include/cub3D.h"
 
-int key_press(int keycode, t_game *game) {
+int	key_press(int keycode, t_game *game)
+{
   if (keycode == XK_w)
     game->player.key_w = true;
   if (keycode == XK_s)
@@ -25,22 +26,27 @@ int key_press(int keycode, t_game *game) {
     game->player.left_turn = true;
   if (keycode == XK_Right)
     game->player.right_turn = true;
-  if (keycode == XK_Tab) {
-    game->player.mouse_captured = !game->player.mouse_captured;
-    if (game->player.mouse_captured) {
-      mlx_mouse_hide(game->mlx, game->win);
-      mlx_mouse_move(game->mlx, game->win, WIDTH / 2, HEIGHT / 2);
-    } else
-      mlx_mouse_show(game->mlx, game->win);
+  if (keycode == XK_Tab) 
+  {
+	game->player.mouse_captured = !game->player.mouse_captured;
+	if (game->player.mouse_captured) 
+	{
+		mlx_mouse_hide(game->mlx, game->win);
+		mlx_mouse_move(game->mlx, game->win, WIDTH / 2, HEIGHT / 2);
+    } 
+	else
+	mlx_mouse_show(game->mlx, game->win);
   }
-  if (keycode == XK_Escape) {
+  if (keycode == XK_Escape) 
+  {
     free_all(game);
     exit(0);
   }
   return (0);
 }
 
-int key_release(int keycode, t_game *game) {
+int	key_release(int keycode, t_game *game)
+{
   if (keycode == XK_w)
     game->player.key_w = false;
   if (keycode == XK_s)
