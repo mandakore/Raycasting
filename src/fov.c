8d80f5f691932cc54633d635f2c69f4c17607d0d
mandakore <galaxyfish11@gmail.com>
2026-03-04 17:06:26 +0900

    fin

--- src/fov.c
+++ src/fov.c
@@ -6,58 +6,58 @@
 /*   By: atashiro <atashiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/04 16:29:06 by atashiro          #+#    #+#             */
-/*   Updated: 2026/03/04 16:36:18 by atashiro         ###   ########.fr       */
+/*   Updated: 2026/03/04 17:03:48 by atashiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3D.h"
 
-static int is_in_fov(double px, double py, double dir_x, double dir_y,
-                     double plane_x, double plane_y) 
+static int	is_in_fov(double x, double y, double *dir, double *pl)
 {
-	double left_x;
-	double left_y;
-	double right_x;
-	double right_y;
-	double cross_l;
-	double cross_r;
+	double	left_x;
+	double	left_y;
+	double	right_x;
+	double	right_y;
 
-	left_x = dir_x - plane_x;
-	left_y = dir_y - plane_y;
-	right_x = dir_x + plane_x;
-	right_y = dir_y + plane_y;
-	cross_l = px * left_y - py * left_x;
-	cross_r = px * right_y - py * right_x;
-	if (cross_l <= 0 && cross_r >= 0)
+	left_x = dir[0] - pl[0];
+	left_y = dir[1] - pl[1];
+	right_x = dir[0] + pl[0];
+	right_y = dir[1] + pl[1];
+	if (x * left_y - y * left_x <= 0 && x * right_y - y * right_x >= 0)
 		return (1);
 	return (0);
 }
 
+static void	render_fov_pixels(t_game *game, double *dir, double *pl, int *c)
+{
+	int	x;
+	int	y;
+	int	r;
+
+	r = MM_RAD * WALL;
+	y = -r - 1;
+	while (++y <= r)
+	{
+		x = -r - 1;
+		while (++x <= r)
+		{
+			if (x * x + y * y <= r * r && is_in_fov(x, y, dir, pl))
+				blend_pixel(c[0] + x, c[1] + y, game);
+		}
+	}
+}
+
 void	draw_fov(t_game *game)
 {
-	int		px;
-	int		py;
-	int		center_x;
-	int		center_y;
-	double	dir_x;
-	double	dir_y;
-	int		radius;
+	double	dir[2];
+	double	pl[2];
+	int		c[2];
 
-  center_x = MM_PLAYER_X + MM_RAD * WALL + (WALL - 6) / 4;
-  center_y = MM_PLAYER_Y + MM_RAD * WALL + (WALL - 6) / 4;
-  dir_x = cos(game->player.dire);
-  dir_y = sin(game->player.dire);
-  radius = MM_RAD * WALL;
-  py = -radius;
-	while (py <= radius)
-	{
-    px = -radius;
-		while (px <= radius)
-		{
-			if (px * px + py * py <= radius * radius && is_in_fov((double)px, (double)py, dir_x, dir_y, -dir_y * 0.66, dir_x * 0.66))
-        blend_pixel(center_x + px, center_y + py, game);
-      px++;
-    }
-    py++;
-  }
+	c[0] = MM_PLAYER_X + MM_RAD * WALL + (WALL - 6) / 4;
+	c[1] = MM_PLAYER_Y + MM_RAD * WALL + (WALL - 6) / 4;
+	dir[0] = cos(game->player.dire);
+	dir[1] = sin(game->player.dire);
+	pl[0] = -dir[1] * 0.66;
+	pl[1] = dir[0] * 0.66;
+	render_fov_pixels(game, dir, pl, c);
 }
